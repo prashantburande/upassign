1bd93e8bb547c0ba8ac2767e9fe8405bada2d45f
prashantburande <101568499+prashantburande@users.noreply.github.com>
2022-03-30 16:42:22 +0530

    Add files via upload

---
+++ Day1_2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+
+// Global function defination 
+void print( void  )// called function 
+{
+    printf("Hello OM25 \n"); 
+}
+int main()  // calling function 
+{
+    
+    print( ); // function call 
+    return 0;
+}
+
+
+ 
